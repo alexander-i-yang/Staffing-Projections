63bcfaa40cd9b46dba69d1d631c0b307f27b6d9b
theuncleofAlex <alex@yang2k.com>
2021-02-02 23:39:42 -0600

    More flexible date range support, checked half-hour capabilities

--- InvFactorial.py
+++
@@ -1,9 +0,0 @@
-from math import factorial
-
-def ErlangC(A, N):
-    L = (A**N / factorial(N)) * (N / (N - A))
-    sum_ = 0
-    for i in range(N):
-        sum_ += (A**i) / factorial(i)
-    return (L / (sum_ + L))
-
